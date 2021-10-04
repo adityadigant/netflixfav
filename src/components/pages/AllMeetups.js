998daffe662cfb140d0516edff3e7b0541c1bd75
adityadigant <adityadigant96@yahoo.com>
2021-10-04 21:14:26 +0530

    except favourite

--- src/components/pages/AllMeetups.js
+++ src/components/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -22,10 +23,40 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetUpsPage() {
+ const [isLoading, setIsLoading] = useState(true); 
+ const [loadedMeetups, setLoadedMeetups] = useState([])
+
+useEffect(()=>{
+  setIsLoading(true);
+  fetch("https://meetup-12fac-default-rtdb.firebaseio.com/meetup.json")
+  .then(Response=>{
+    return Response.json()
+  }).then(data =>{
+    const meetups = [];
+
+    for(const key in data){
+      const meetup = {
+        id:key,
+        ...data[key]
+      };
+      meetups.push(meetup)
+    }
+    console.log(data)
+    setIsLoading(false)
+    setLoadedMeetups(meetups)
+  })
+},[])
+ 
+
+  if(isLoading){
+    return(<section>
+      <p>Loading...</p>
+    </section>)
+  }
   return (
     <section>
       <h1>All Meet Ups</h1>
-      <MeetupList meetups={DUMMY_DATA}/>
+      <MeetupList meetups={loadedMeetups}/>
     </section>
   );
 }
